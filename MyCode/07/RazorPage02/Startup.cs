cab553efa510ddd0116b9a0925d1e0b40b118b05
Wagner Xu <wagnerhsu@hotmail.com>
2020-11-04 09:33:57 +0800

    Change project structure

--- MyCode/07/RazorPage02/Startup.cs
+++ MyCode/07/RazorPage02/Startup.cs
@@ -22,7 +22,7 @@ namespace RazorPage02
             {
                 options.UseInMemoryDatabase("name");
             });
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
